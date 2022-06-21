b59f8fe51c9b3e47f6dd04934e42990723056bc5
paulweezydesign <weezy@paulweezydesign.com>
2022-06-21 16:08:48 -0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-  <nav
-    class="
-      grid grid-cols-2
-      w-[100vw]
-      items-center
-      px-[8%]
-      py-2
-      lg:py-4
-      bg-slate-100
-    "
-  >
+  <nav class="grid grid-cols-2 w-[100vw] items-center px-[8%] py-2 lg:py-4">
     <div class="">
       <a class="">
         <h1 class="text-xl font-bold">Vue App</h1>
       </a>
     </div>
-    <div class="flex justify-end">
+    <div class="flex lg:hidden justify-end">
       <Sidebar />
+    </div>
+    <div class="hidden lg:flex justify-end">
+      <a class="uppercase mr-8" href="/">Home</a>
+      <a class="uppercase mr-8" href="/about">About</a>
+      <a class="uppercase " href="/contact">Contact</a>
     </div>
   </nav>
 </template>
