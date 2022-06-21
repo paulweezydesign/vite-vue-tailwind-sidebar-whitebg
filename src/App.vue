8f83cbfa3c53a01bca13e08acd95551dfd6e43a3
paulweezydesign <weezy@paulweezydesign.com>
2022-06-21 15:18:06 -0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <Sidebar />
+  <nav class="grid grid-cols-2 w-[100vw] items-center px-[8%] py-2 bg-slate-100">
+    <div class="">
+      <a class="">
+        <h1 class="text-xl font-bold">Vue App</h1>
+      </a>
+    </div>
+    <div class="flex justify-end">
+      <Sidebar />
+    </div>
+  
+  </nav>
 </template>
 
 <script setup>
